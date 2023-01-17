713f2404f9072fa639edd3d7db72a4f02e6232f2
LG <lechu131313@gmail.com>
2023-01-17 19:55:58 +0100

    added -dark/light theme - multilingual

--- js/main.js
+++ js/main.js
@@ -134,3 +134,45 @@ function playStop() {
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
 })
+
+function toggleTheme() {
+    let mode = document.getElementById('mode');
+    
+    if (mode.innerHTML === 'dark_mode') {
+        mode.innerHTML = 'light_mode';
+    } else {
+        mode.innerHTML = 'dark_mode';
+    }
+} 
+
+// ******* Multilingual *******
+
+
+let language = document.querySelector(".language"),
+     link = document.querySelectorAll("a"),
+     logo = document.querySelector(".logo"),
+     login = document.querySelector(".login");
+
+link.forEach(el=>{
+     el.addEventListener("click", ()=>{
+          language.querySelector(".active").classList.remove("active");
+          el.classList.add("active");
+
+          let attr = el.getAttribute("language")
+
+          logo.textContent = languageData[attr].logo
+          login.textContent = languageData[attr].login
+     })
+})
+// -------- Language DATA -----------
+let languageData = {
+     polish: {
+          logo: "II Wojna Światowa w filmach",
+          login: "Logowanie / Rejestracja",
+        },
+        english: {
+            logo: "World War II Movies",
+            login: "Login / Signup",
+     }
+}
+//---------------------------------------END Language DATA
