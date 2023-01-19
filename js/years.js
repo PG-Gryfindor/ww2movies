ffd97420a9223912e159dfc0f88f01b620b1aaab
LG <lechu131313@gmail.com>
2023-01-19 21:32:13 +0100

    bez removeMap

--- js/years.js
+++ js/years.js
@@ -8,19 +8,21 @@
 // ******* Switching Years_nav *******
 const changingBG = document.querySelector('.changeBG')
 
+
  function changeTo1939() {
         changingBG.style.background = `url(./img/bg-1939.jpg)`;
         layerID = 'movies' ;
         sourceLayer = './json/movies.json';
-        deleteMap();
+        
         reloadMap();
+        
 }
 
  function changeTo1940() {
         changingBG.style.background = `url(./img/bg-1940.jpg)`;
          layerID = '1944';
          sourceLayer = './json/1944.json';
-         deleteMap();
+        
          reloadMap();
 }
 
@@ -55,6 +57,8 @@ const changingBG = document.querySelector('.changeBG')
 }
 
 function reloadMap () {
+
+
 // ******* Map ******* 
 const key = 'KBNcLm3O2HsmQwAGcJSS ';
 const map = new maplibregl.Map({
@@ -62,6 +66,7 @@ const map = new maplibregl.Map({
   style: `https://api.maptiler.com/maps/70623a5f-370e-4bc5-8281-3492c6297486/style.json?key=KBNcLm3O2HsmQwAGcJSS`, // style URL
   center: [0.0, 45.2125578], // starting position [lng, lat]
   zoom: 1.9, // starting zoom
+
 });
 map.addControl(new maplibregl.NavigationControl(), 'top-right');
 
@@ -111,64 +116,72 @@ map.on('click', layerID, function (e) {
                     <a href = "${e.features[0].properties.filmweb}">filmweb</a>`)
         .addTo(map);
 });
+
+
+   // map.remove();
+
+
 };
+////////////////////////DELETE
+// function deleteMap () {
+//     // ******* Map ******* 
+//     const key = 'KBNcLm3O2HsmQwAGcJSS ';
+// const map = new maplibregl.Map({
+//   container: 'map', // container's id or the HTML element in which MapLibre GL JS will render the map
+//   style: `https://api.maptiler.com/maps/70623a5f-370e-4bc5-8281-3492c6297486/style.json?key=KBNcLm3O2HsmQwAGcJSS`, // style URL
+//   center: [0.0, 45.2125578], // starting position [lng, lat]
+//   zoom: 2000, // starting zoom
+// });
+//    // map.addControl(new maplibregl.NavigationControl(), 'top-right');
+    
+//      // ******* Maps Markers *******
+//     // map = null;
 
-
-function deleteMap () {
-    // ******* Map ******* 
-    const key = 'KBNcLm3O2HsmQwAGcJSS ';
-    const map = new maplibregl.Map({
-      container: 'map', // container's id or the HTML element in which MapLibre GL JS will render the map
-      style: `https://api.maptiler.com/maps/70623a5f-370e-4bc5-8281-3492c6297486/style.json?key=KBNcLm3O2HsmQwAGcJSS`, // style URL
-      center: [0.0, 45.2125578], // starting position [lng, lat]
-      zoom: 1.9, // starting zoom
-    });
-    map.addControl(new maplibregl.NavigationControl(), 'top-right');
-    
-     // ******* Maps Markers *******
-     
     
     
-     map.off('load', function () {
-        // Add an image to use as a custom marker
-        map.loadImage(
-            './img/pin.png',
-            function (error, image) {
-                if (error)
-                    throw error;
-                map.addImage('custom-marker', image);
-                map.removeSource(layerID, {
-                    'type': 'geojson',
-                    'data': sourceLayer
-                });
-                // Add a symbol layer
-                map.removeLayer({
-                        'id': layerID,
-                        'type': 'symbol',
-                        'source': layerID,
-                        'layout': {
-                            'icon-image': 'custom-marker',
-                            'icon-size': 0.06,
-                            'icon-overlap': 'always'
-                        }
-                    });
+//      map.off('load', function () {
+//         // Add an image to use as a custom marker
+//         map.removeImage(
+//             './img/pin.png',
+//             function (error, image) {
+//                 if (error)
+//                     throw error;
+//                 map.removeImage('custom-marker', image);
+//                 map.removeSource(layerID, {
+//                     'type': 'geojson',
+//                     'data': sourceLayer
+//                 });
+//                 // Add a symbol layer
+//                 map.removeLayer({
+//                         'id': layerID,
+//                         'type': 'symbol',
+//                         'source': layerID,
+//                         'layout': {
+//                             'icon-image': 'custom-marker',
+//                             'icon-size': 0.06,
+//                             'icon-overlap': 'always'
+//                         }
+//                     });
                 
-            });
-    });
+//             });
+//     });
     
     
     
-    map.off('click', layerID, function (e) {
-        new maplibregl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML(`<h3>TITLE</h3>${e.features[0].properties.title}
-                    <h3>YEAR OF PRODUCTION</h3>${e.features[0].properties.yearProduction}
-                    <h3>YEAR OF ACTION</h3>${e.features[0].properties.yearAction}
-                    <h3>CAST</h3>${e.features[0].properties.cast}
-                    <h3>DESCRIPTION</h3>${e.features[0].properties.description}
-                    <h3>LINKS\n</h3>
-                        <a href = "${e.features[0].properties.imdb}">IMDB</a><br> 
-                        <a href = "${e.features[0].properties.filmweb}">filmweb</a>`)
-            .removeFrom(map);
-    });
-    };
+//     map.off('click', layerID, function (e) {
+//         new maplibregl.Popup()
+//             .setLngLat(e.lngLat)
+//             .setHTML(`<h3>TITLE</h3>${e.features[0].properties.title}
+//                     <h3>YEAR OF PRODUCTION</h3>${e.features[0].properties.yearProduction}
+//                     <h3>YEAR OF ACTION</h3>${e.features[0].properties.yearAction}
+//                     <h3>CAST</h3>${e.features[0].properties.cast}
+//                     <h3>DESCRIPTION</h3>${e.features[0].properties.description}
+//                     <h3>LINKS\n</h3>
+//                         <a href = "${e.features[0].properties.imdb}">IMDB</a><br> 
+//                         <a href = "${e.features[0].properties.filmweb}">filmweb</a>`)
+//             .removeFrom(map);
+//     });
+
+
+    
+//     };
