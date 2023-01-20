9651bd3449d8f16ff7d815f53ef0c373f684197f
LG <lechu131313@gmail.com>
2023-01-20 14:11:28 +0100

    added lang-trans, @media

--- js/languages.js
+++ js/languages.js
@@ -6,7 +6,13 @@
 let language = document.querySelector(".language"),
      link = document.querySelectorAll(".lang"),
      logo = document.querySelector(".logo"),
-     login = document.querySelector(".login");
+     login = document.querySelector(".login"),
+     form__title_login = document.querySelector(".form__title_login"),
+     form__button = document.querySelector(".form__button"),
+     forgot = document.querySelector(".forgot"),
+     create = document.querySelector(".create"),
+     already = document.querySelector(".already"),
+     form__title_create = document.querySelector(".form__title_create");
 
 link.forEach(el=>{
      el.addEventListener("click", ()=>{
@@ -17,6 +23,12 @@ link.forEach(el=>{
 
           logo.textContent = languageData[attr].logo
           login.textContent = languageData[attr].login
+          form__title_login.textContent = languageData[attr].form__title_login
+          form__button.textContent = languageData[attr].form__button
+          forgot.textContent = languageData[attr].forgot
+          create.textContent = languageData[attr].create
+          already.textContent = languageData[attr].already
+          form__title_create.textContent = languageData[attr].form__title_create
      })
 })
 // -------- Language DATA -----------
@@ -24,10 +36,22 @@ let languageData = {
      polish: {
           logo: "II Wojna Światowa w filmach",
           login: "Logowanie / Rejestracja",
-        },
-        english: {
-            logo: "World War II Movies",
-            login: "Login / Signup",
+          form__title_login: "Logowanie",
+          form__button: "Kontynuuj",
+          forgot: "Nie pamiętasz hasła?",
+          create: "Nie masz jeszcze konta? Zarejestruj się",
+          form__title_create: "Utwórz Konto",
+          already: "Masz już konto? Zaloguj się"
+     },
+     english: {
+          logo: "World War II Movies",
+          login: "Login / Signup",
+          form__title_login: "Login",
+          form__button: "Continue",
+          forgot: "Forgot your password?",
+          create: "Don't have account? Create account",
+          form__title_create: "Create Account",
+          already: "Already have an account? Sign in"
      }
 }
 //---------------------------------------END Language DATA
